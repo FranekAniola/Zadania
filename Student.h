9752e0d41bf3c6a134917d7b0da078d9f074d9e0
Student <s25532@pjwstk.edu.pl>
2021-11-19 11:22:27 +0100

    student with header file

---
+++ Student.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+struct Student{
+	private: 
+	std::string name;
+	std::string surname;
+	std::string index;
+	int semester;
+	float avarage;
+	
+	public:	
+	Student(std::string n, std::string sn, std::string i, int sm, float ag)
+	{
+		name = n;
+		surname = sn;
+		index = i;
+		semester = sm;
+		avarage = ag;
+		
+	}	
+
+
+	auto const to_string() -> std::string
+    {
+    auto result = std::ostringstream{};
+	result << name << " " <<surname << " ("<<index<<") semester: " << semester << " avarage grade: "<< avarage;
+	
+	return result.str();    
+    }
+};

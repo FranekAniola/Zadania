69bb0f3fee59f7e428a4fce1bb80dbead80f2f62
FranekAniola <s25532@pjwstk.edu.pl>
2021-11-30 01:52:04 +0100

    repaird vector counter

--- listOfStudents.cpp
+++ listOfStudents.cpp
@@ -48,20 +48,23 @@ struct Student{
 	}
 	
     void pushing(Student student){
-
+		/*
 		std::vector<Student>vectorStudent;
 		vectorStudent.push_back(student);
 		int vSize = vectorStudent.size();
 		vSize++;
 		std::cout<<"List of size is: "<<vSize<<"\n";
+		*/
 	}
 	
 	
 auto main() -> int
 {
+	std::vector<Student>vectorStudent;
 	std::string n, sn, i;
-	int sm, choice;
-	
+	int sm, choice, vSize;
+	vSize = vectorStudent.size();
+
 	do{
 	
 	std::cout<<"1. Adding a new student \n";
@@ -75,8 +78,12 @@ auto main() -> int
 		
 		switch(choice){
 		case 1:
-		auto s1 = Student{name(n),surname(sn), index(i), semester(sm)};
-		pushing(s1);
+		
+		auto s1 = Student{name(n),surname(sn), index(i), semester(sm)};		
+		vectorStudent.push_back(s1);
+		vSize++;
+		
+		std::cout<<"Size of list of Students is: "<<vSize<<"\n";
 		break;
 		}
 		

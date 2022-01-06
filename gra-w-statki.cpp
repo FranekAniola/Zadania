32734a2e9f833545a4978f645b1b09c2c981f2d4
FranekAniola <s25532@pjwstk.edu.pl>
2022-01-06 22:28:07 +0100

    wczytywanie parametrow z consoli

--- gra-w-statki.cpp
+++ gra-w-statki.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 #include <vector>
 
+/*
+auto ships(std::vector row, std::vector collumn, int argc, char* argv[]){
 
-//auto threeMastedShipAxis(char* [])
+	std::string firstShip = atoi(argv[1]);
+	
+	for(int i = 0; i < 10;++i){
+		
+		if(firstShip[0] == atoi(row) && firstShip[1] == atoi(collumn)){
+			
+		}
+	}
+
+}
+*/
 
 
-auto main(int argc, char* argv[]) ->int
+
+auto main(int argc, char *argv[]) ->int
 {
 	
 	
@@ -15,9 +29,7 @@ auto main(int argc, char* argv[]) ->int
         {'.', '.', '.','.', '.', '.','.', '.', '.','.'}, //1
         {'.', '.', '.','.', '.', '.','.', '.', '.','.'}, //2
         {'.', '.', '.','.', '.', '.','.', '.', '.','.'}, //3
-        {'.', '.', '.','.', '.', '.','.', '.', '.','.'}, //3
         {'.', '.', '.','.', '.', '.','.', '.', '.','.'}, //4
-        {'.', '.', '.','.', '.', '.','.', '.', '.','.'}, //5
         {'.', '.', '.','.', '.', '.','.', '.', '.','.'}, //5
         {'.', '.', '.','.', '.', '.','.', '.', '.','.'}, //6
         {'.', '.', '.','.', '.', '.','.', '.', '.','.'}, //7
@@ -28,16 +40,43 @@ auto main(int argc, char* argv[]) ->int
     
     
     
-    auto row = std::vector<std::string>{"A","B","C","D","E","F","G","H","I","J"};
+    auto row = std::vector<char>{'A','B','C','D','E','F','G','H','I','J'};
     auto collumn = std::vector<int>{0,1,2,3,4,5,6,7,8,9};
+    
+    for(int i = 0;i < collumn.size();++i){
+		std::cout<< "   " << collumn[i];
+	}
+	
+	std::cout<<std::endl;
 	
 	for(int i = 0; i < vec.size();++i){
 	std::cout<<row[i];
 		for(int j = 0;j < vec[i].size();++j){
+			
 		std::cout<<'|' << vec[i][j] << "| ";
 		}
+		
 	std:: cout << std::endl;
+	
 	}
+	
+	std::string firstShip;
+	
+	firstShip = std::string(argv[1]);
+	
+	
+	
+	for(int i = 0; i < 10;++i){
+		
+		if(firstShip[0] == row[i] && firstShip[1] == char(collumn[i])){
+			
+			std::cout<<"jest!";		
+		}
+	}
+	/*
+	std::cout<< "\n" <<argv[1] << "\n" ;		
+	std::cout<<firstShip[1] << "\n";
+	*/
 	
 	return 0;
 }

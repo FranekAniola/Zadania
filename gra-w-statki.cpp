e8b7001908c00ea6458b3e02090664e1280c509e
FranekAniola <s25532@pjwstk.edu.pl>
2022-01-07 15:25:34 +0100

    funkcja bool is Input Valid

--- gra-w-statki.cpp
+++ gra-w-statki.cpp
@@ -3,21 +3,19 @@
 #include <algorithm>
 #include <vector>
 
-/*
-auto ships(std::vector row, std::vector collumn, int argc, char* argv[]){
 
-	std::string firstShip = atoi(argv[1]);
-	
-	for(int i = 0; i < 10;++i){
+auto IsInputValid(std::string ship, std::vector<char>& row, std::vector<int>& collumn) -> bool{
+	bool isEqualK = isdigit(ship[1]);
+	bool isEqualR = (ship[0] >= 'A') && (ship[0] <= 'J');
 		
-		if(firstShip[0] == atoi(row) && firstShip[1] == atoi(collumn)){
-			
-		}
+	if(isEqualK && isEqualR) {
+		std::cout<<"Correct parameters";
+	} else {
+		std::cout<<"Wrong parameters";
 	}
 
+	return isEqualK && isEqualR;
 }
-*/
-
 
 
 auto main(int argc, char *argv[]) ->int
@@ -44,39 +42,32 @@ auto main(int argc, char *argv[]) ->int
     auto collumn = std::vector<int>{0,1,2,3,4,5,6,7,8,9};
     
     for(int i = 0;i < collumn.size();++i){
-		std::cout<< "   " << collumn[i];
+		std::cout<< " " << collumn[i] << "  "; 
 	}
 	
 	std::cout<<std::endl;
 	
 	for(int i = 0; i < vec.size();++i){
-	std::cout<<row[i];
+		
 		for(int j = 0;j < vec[i].size();++j){
 			
 		std::cout<<'|' << vec[i][j] << "| ";
 		}
+		std::cout<<row[i];
+		std:: cout << std::endl;
 		
-	std:: cout << std::endl;
-	
 	}
 	
 	std::string firstShip;
 	
 	firstShip = std::string(argv[1]);
 	
+	IsInputValid(firstShip,row,collumn);
 	
-	
-	for(int i = 0; i < 10;++i){
-		
-		if(firstShip[0] == row[i] && firstShip[1] == char(collumn[i])){
-			
-			std::cout<<"jest!";		
-		}
-	}
-	/*
 	std::cout<< "\n" <<argv[1] << "\n" ;		
-	std::cout<<firstShip[1] << "\n";
-	*/
+	//std::cout<<firstShip[0] << "\n";
+	//std::cout<<firstShip[1] << "\n";
+	
 	
 	return 0;
 }

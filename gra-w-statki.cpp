f1eb960a2f9b6990983ba82c1c9481d8345227d0
FranekAniola <s25532@pjwstk.edu.pl>
2022-01-09 14:22:23 +0100

    arranging ships, adding vertical and horizontal position

--- gra-w-statki.cpp
+++ gra-w-statki.cpp
@@ -4,23 +4,50 @@
 #include <vector>
 
 
-auto IsInputValid(std::string ship, std::vector<char>& row, std::vector<int>& collumn) -> bool{
+auto IsInputValid(int& rowInt, int& x,std::string ship, std::vector<char>& row, std::vector<int>& collumn) -> bool {
+	char* secondCharacter = &ship[1];
+
 	bool isEqualK = isdigit(ship[1]);
+	
 	bool isEqualR = (ship[0] >= 'A') && (ship[0] <= 'J');
-		
+	
 	if(isEqualK && isEqualR) {
-		std::cout<<"Correct parameters";
+		std::cout<<"\n Correct parameters";
+		x = atoi(secondCharacter);
 	} else {
 		std::cout<<"Wrong parameters";
 	}
-
+	
+	for(int i = 0; i <10; ++i) {
+		if(ship[0] == row[i]) {
+			rowInt = i;
+			break;
+		}
+	}
+	
 	return isEqualK && isEqualR;
 }
 
 
-auto main(int argc, char *argv[]) ->int
-{
+
+auto PrintBoard(std::vector<char>& row, std::vector<int>& collumn, std::vector<std::vector<char>>& vec) -> void {
 	
+	for(int i = 0;i < collumn.size();++i) {
+		std::cout<< " " << collumn[i] << "  "; 
+	}
+	
+	std::cout<<std::endl;
+	
+	for(int i = 0; i < vec.size();++i) {
+		for(int j = 0;j < vec[i].size();++j) {
+			std::cout<<'|' << vec[i][j] << "| ";
+		}
+		std::cout<<row[i];
+		std:: cout << std::endl;
+	}
+}
+
+auto main(int argc, char *argv[]) -> int {
 	
 	std::vector<std::vector<char>> vec =
     {
@@ -35,39 +62,55 @@ auto main(int argc, char *argv[]) ->int
         {'.', '.', '.','.', '.', '.','.', '.', '.','.'}, //9
         {'.', '.', '.','.', '.', '.','.', '.', '.','.'}, //10
     };
-    
-    
-    
-    auto row = std::vector<char>{'A','B','C','D','E','F','G','H','I','J'};
+      
+    auto row = std::vector<char> {'A','B','C','D','E','F','G','H','I','J'};
     auto collumn = std::vector<int>{0,1,2,3,4,5,6,7,8,9};
+    auto rowToNumbers = std::vector<int>{};
     
-    for(int i = 0;i < collumn.size();++i){
-		std::cout<< " " << collumn[i] << "  "; 
-	}
+  
+    
+    PrintBoard(row, collumn, vec);
 	
-	std::cout<<std::endl;
-	
-	for(int i = 0; i < vec.size();++i){
-		
-		for(int j = 0;j < vec[i].size();++j){
-			
-		std::cout<<'|' << vec[i][j] << "| ";
-		}
-		std::cout<<row[i];
-		std:: cout << std::endl;
-		
-	}
+	int x;
+	int rowInt;
+	char v = 'v';
+	char h = 'h';
 	
 	std::string firstShip;
+	std::string secondShip;
 	
 	firstShip = std::string(argv[1]);
+	secondShip = std::string(argv[2]);
 	
-	IsInputValid(firstShip,row,collumn);
 	
-	std::cout<< "\n" <<argv[1] << "\n" ;		
-	//std::cout<<firstShip[0] << "\n";
-	//std::cout<<firstShip[1] << "\n";
+
+
+	char ShipField = 'x';
 	
+	if(argv[1] && firstShip[2] == h) {
+		IsInputValid(rowInt,x,firstShip,row,collumn);
+		std::cout<<std::endl;
+		
+		for(int i = 0; i < 4;++i) {
+			vec[rowInt][x+i] = ShipField;
+		}
+		
+	} else if(argv[1] && firstShip[2] == v) {
+		IsInputValid(rowInt,x,firstShip,row,collumn);
+		std::cout<<std::endl;
+		
+		for(int i = 0; i < 4;++i) {
+			vec[rowInt+i][x] = ShipField;
+		}
+	
+	}
+	
+	IsInputValid(rowInt,x,secondShip,row,collumn);
+	std::cout<<std::endl;
+	
+	vec[rowInt][x] = ShipField;
+	
+	PrintBoard(row, collumn, vec);
 	
 	return 0;
 }
